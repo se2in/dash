a21b507718e66981b44cb0d4c9b2440400b7d68c
sanghyun ahn <dkstkd840425@gmail.com>
2026-04-29 08:12:39 +0900

    dashboard auto update: 2026-04-29 08:09:38

--- data.js
+++ data.js
@@ -1,20 +1,20 @@
 window.dashboardData = {
-  "sp500": "NA",
-  "sp500_chg": "-",
-  "vix": "NA",
-  "vix_chg": "-",
-  "gold": "NA",
-  "gold_chg": "-",
-  "oil": "NA",
-  "oil_chg": "-",
-  "btc": "NA",
-  "btc_chg": "-",
-  "usdkrw": "NA",
-  "usdkrw_chg": "-",
+  "sp500": "7,138.80",
+  "sp500_chg": "-0.49% vs prev close",
+  "vix": "17.83",
+  "vix_chg": "-1.05% vs prev close",
+  "gold": "4,607.40",
+  "gold_chg": "-1.45% vs prev close",
+  "oil": "99.61",
+  "oil_chg": "+3.36% vs prev close",
+  "btc": "76,277.75",
+  "btc_chg": "-1.41% vs prev close",
+  "usdkrw": "1,473.00",
+  "usdkrw_chg": "-0.11% vs prev close",
   "yield_spread": "-",
   "yield_spread_chg": "-",
-  "fear_greed": "-",
-  "fear_greed_label": "unavailable",
+  "fear_greed": "64",
+  "fear_greed_label": "greed",
   "options_expiry": "2026-05-01",
   "options_note": "주간 만기",
   "cpi_yoy": "-",
@@ -23,25 +23,117 @@ window.dashboardData = {
   "nfp_date": "-",
   "next_cpi": "-",
   "next_nfp": "-",
-  "nextrade_basis": "기준 정보 -",
+  "nextrade_basis": "2026-04-29\n08:11\n기준 정보\nNXT 정규시장 체결기준",
   "nextrade_session": "08:00~08:50",
-  "nextrade_change": "-",
-  "nextrade_issues": "-",
-  "nextrade_volume": "-",
-  "nextrade_value": "-",
-  "nextrade_note": "클릭 시 넥스트레이드 열기",
+  "nextrade_change": "-1.06%",
+  "nextrade_issues": "607",
+  "nextrade_volume": "30,315,279",
+  "nextrade_value": "2,492,341,015,510",
+  "nextrade_note": "* 등락률은 기준시점 대비 시가총액의 증감률임",
   "fedwatch_hold": "동결 -",
   "fedwatch_hike": "변경 -",
   "fedwatch_date": "클릭 시 CME FedWatch 열기",
-  "doughcon_level": "DOUGHCON 3",
+  "doughcon_level": "DOUGHCON 4",
   "doughcon_location": "ROUND HOUSE",
-  "doughcon_status": "INCREASE IN FORCE READINESS",
+  "doughcon_status": "ELEVATED WATCH",
   "doughcon_note": "클릭 시 PizzINT 열기",
-  "earnings": [],
+  "earnings": [
+    {
+      "date": "2026-04-29",
+      "symbol": "AMZN",
+      "company": "Amazon.com, Inc.",
+      "eps_estimate": "1.65",
+      "time": "AMC"
+    },
+    {
+      "date": "2026-04-29",
+      "symbol": "GOOGL",
+      "company": "Alphabet Inc.",
+      "eps_estimate": "2.67",
+      "time": "AMC"
+    },
+    {
+      "date": "2026-04-29",
+      "symbol": "META",
+      "company": "Meta Platforms, Inc.",
+      "eps_estimate": "6.65",
+      "time": "AMC"
+    },
+    {
+      "date": "2026-04-29",
+      "symbol": "MSFT",
+      "company": "Microsoft Corporation",
+      "eps_estimate": "4.07",
+      "time": "AMC"
+    },
+    {
+      "date": "2026-04-29",
+      "symbol": "QCOM",
+      "company": "QUALCOMM Incorporated",
+      "eps_estimate": "2.56",
+      "time": "AMC"
+    },
+    {
+      "date": "2026-04-30",
+      "symbol": "AAPL",
+      "company": "Apple Inc.",
+      "eps_estimate": "1.94",
+      "time": "AMC"
+    },
+    {
+      "date": "2026-05-01",
+      "symbol": "CVX",
+      "company": "Chevron Corporation",
+      "eps_estimate": "0.97",
+      "time": "BMO"
+    },
+    {
+      "date": "2026-05-01",
+      "symbol": "XOM",
+      "company": "Exxon Mobil Corporation",
+      "eps_estimate": "0.99",
+      "time": "BMO"
+    },
+    {
+      "date": "2026-05-04",
+      "symbol": "PLTR",
+      "company": "Palantir Technologies Inc.",
+      "eps_estimate": "0.28",
+      "time": "AMC"
+    },
+    {
+      "date": "2026-05-05",
+      "symbol": "AMD",
+      "company": "Advanced Micro Devices, Inc.",
+      "eps_estimate": "1.28",
+      "time": "AMC"
+    },
+    {
+      "date": "2026-05-05",
+      "symbol": "PFE",
+      "company": "Pfizer, Inc.",
+      "eps_estimate": "0.72",
+      "time": "BMO"
+    },
+    {
+      "date": "2026-05-06",
+      "symbol": "DIS",
+      "company": "Walt Disney Company (The)",
+      "eps_estimate": "1.49",
+      "time": "BMO"
+    },
+    {
+      "date": "2026-05-06",
+      "symbol": "UBER",
+      "company": "Uber Technologies, Inc.",
+      "eps_estimate": "0.71",
+      "time": "BMO"
+    }
+  ],
   "fnguide_reports": [],
   "ystreet_title": "YStreet ETF Tracker",
   "ystreet_url": "https://d1rjfvjf23ngm5.cloudfront.net/#dashboard",
-  "ystreet_note": "YStreet ETF Tracker 주요 지표를 반영했습니다.",
+  "ystreet_note": "ActiveETF Tracker 대시보드 직접 경로를 사용합니다.",
   "ystreet_market_buy": "-",
   "ystreet_market_sell": "-",
   "ystreet_market_buy_label": "매수 우위",
@@ -53,5 +145,5 @@ window.dashboardData = {
   "ystreet_buy_top10": [],
   "ystreet_sell_top10": [],
   "fnguide_url": "https://comp.fnguide.com/SVO2/ASP/SVD_Report_Summary.asp?pGB=1&gicode=A005930&cID=&MenuYn=Y&ReportGB=&NewMenuID=901&stkGb=701",
-  "updated_at": "2026-04-28 07:50:19"
+  "updated_at": "2026-04-29 08:12:38"
 };
